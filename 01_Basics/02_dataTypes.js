63e9ca965669854d2dcb07a420a613448a8ac642
Gautam Yadav <gautamyadav1567@gmail.com>
2026-07-06 16:52:41 +0000

    Data Type

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3);  we are using node js not browser
+
+console.log(3+
+    3); //Code readability should be high
+
+console.log("Gautam")
+
+let name = "Gautam";
+let age = 25;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bolean => true/false
+// null => stand alone value
+// undefined => value not assigned yet
+// symbol => unique
+
+
+//object
+
+console.log(typeof null); // Object
+console.log(typeof undefined) // undefined.
