4785b68f02ea331f66d25a2a3a5bc07ca661e767
Gautam Yadav <gautamyadav1567@gmail.com>
2026-07-06 17:51:24 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33aa"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "23qa" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggenIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggenIn);
+
+
+// 1 => true; 0 => false
+// " " => false
+// "Gautam" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof(someNumber));
