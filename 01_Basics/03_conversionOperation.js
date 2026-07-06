2defc53fb150fc008663f5154bde37eaa8072560
Gautam Yadav <gautamyadav1567@gmail.com>
2026-07-06 18:13:05 +0000

    conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -48,4 +48,21 @@ let negValue = -value;
 let str1 = "Gautam"
 let str2 = "Yadav"
 
-console.log(str1+str2);
+// console.log(str1+str2);
+
+// console.log("2" + 2)
+// console.log(2 + "2")
+// console.log("2" + 2 + 2)
+// console.log(2+2+"5")
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
